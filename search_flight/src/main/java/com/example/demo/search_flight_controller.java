a94fbfee01a91d72b59efcb9268835b6822649c9
bhallu <bhallu@LIN25007673.corp.capgemini.com>
2021-12-07 15:29:49 +0530

    Updated

---
+++ search_flight/src/main/java/com/example/demo/search_flight_controller.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
+public class search_flight_controller {
+	@Autowired
+	repo rp;
+	@GetMapping("/{route}/{date}")
+	public List<flight_info> getdata(@PathVariable String route,@PathVariable String date){		
+		return rp.findflight(route,date);
+	}
+	@PostMapping("/add")
+	public String getdata(@RequestBody flight_info f1){		
+		try {
+			rp.insert(f1);
+			return "sucessful insert";
+		}catch(Exception e) {
+			return "sorry unable to insert";
+		}
+	}
+	@GetMapping("/hello/{fid}")
+	public String getfrd(@PathVariable String fid) {
+		flight_info f1=rp.getrd(fid);
+		return f1.getRoute()+","+f1.getDate();
+	}
+}
