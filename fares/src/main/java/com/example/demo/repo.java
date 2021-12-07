2d9df7e6728319d57c255017de6c86f58c130534
bhallu <bhallu@LIN25007673.corp.capgemini.com>
2021-12-07 15:28:44 +0530

    Updated

---
+++ fares/src/main/java/com/example/demo/repo.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface repo extends MongoRepository<fare,String>{
+	@Query("{'fid':?0}")
+	public Optional<fare>getinfo(String fid);
+	@Query("{'fid':?0}")
+	public fare getobj(String fid);
+}
