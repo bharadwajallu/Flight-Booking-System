2486078d0dd97714db16e92913ae1a5f1bb964e2
bhallu <bhallu@LIN25007673.corp.capgemini.com>
2021-12-07 15:24:42 +0530

    Updated

---
+++ booking/src/main/java/com/example/demo/booking_controller.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/booking")
+public class booking_controller {
+	
+@Autowired
+repo rp;
+@Autowired
+service_layer s;
+	@GetMapping("/book/{name}")
+	public String add(@PathVariable String name) {
+		
+		try {
+			book ba=s.add(name);
+			String fid=ba.getfid();
+			book b2=rp.get(fid, name);
+			if(b2==null) {
+				rp.insert(ba);
+				return "ticket is booked."+"\nname:"+name+"\nclass:"+ba.getFclass()+"\npayment:"+ba.getPayment_status();
+			}
+			else
+				if(b2.getfid().contentEquals(fid)&&b2.getName().contentEquals(name)) {
+					return "dear user a ticket is booked already with same name in same flight.check credentials to avoid financial loss";
+				}
+				else
+					return "unusual exit";
+
+			
+		}catch(Exception e) {
+			return e.getMessage();
+		}	
+	}
+	
+@GetMapping("/cancel/{fid}/{name}")
+	public String cancel(@PathVariable String fid,@PathVariable String name){
+		try
+	  	{ 
+		book b1=rp.get(fid, name);
+	  	rp.delete(fid, name);
+	  	b1.setStatus("cancelled"); 
+	  	rp.insert(b1);
+	  	return "booking canceled"; 
+	  }catch(Exception e) { 
+		  return e.getMessage();
+		  }
+	}
+	 
+	 @GetMapping("/mybookings/{name}")
+	 public List<book>getdata(@PathVariable String name){
+		 return rp.get(name);
+	 }		
+}
---
+++ booking/src/main/java/com/example/demo/repo.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface repo extends MongoRepository<book,String>{
+	@Query("{'name':?0}")
+	public List<book> get(String name);
+	
+	@Query("{'fid':?0,'name':?1}")
+	public book get(String fid,String name);
+	
+@Query(value="{'fid' : ?0,'name':?1}", delete = true)
+public book delete(String fid,String name);
+
+}
