6946e72e76a5bc8344c20e32bd1c250ccf0c1b8b
bharadwajallu <33192990+bharadwajallu@users.noreply.github.com>
2022-01-15 13:32:50 +0530

    Delete checkin directory

--- checkin/src/main/java/com/example/demo/repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-public interface repo extends MongoRepository<book,String>{
-@Query("{'name':?0,'fid':?1}")
-book get(String name,String fid);
-}
